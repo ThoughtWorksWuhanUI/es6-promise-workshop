0711782bdf23541225732c86c3ebd234a95a716f
benweizhu <benweizhu@gmail.com>
2017-07-03 15:13:35 +0800

    fetchData

--- app/index.js
+++ app/index.js
@@ -1,21 +1,26 @@
-const condition = 'match';
+function fetchData(URL) {
+  return new Promise(function (resolve, reject) {
+    var req = new XMLHttpRequest();
+    req.open('GET', URL, true);
+    req.onload = function () {
+      if (req.status === 200) {
+        resolve(this.responseText);
+      } else {
+        reject(new Error(req.statusText));
+      }
+    };
+    req.onerror = function () {
+      reject(new Error(req.statusText));
+    };
+    req.send();
+  });
+}
 
-var promise = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    if (condition === 'match') {
-      resolve('done');
-    } else {
-      reject('been reject');
-    }
-  }, 2000);
-});
+var promise = fetchData('https://raw.githubusercontent.com/benweizhu/es6-promise-workshop/master/data/books.json');
 
-promise.then(function (value) {
-  var messageEle = document.getElementById('message');
-  messageEle.style.color = 'green';
-  messageEle.innerText = value;
-}).catch(function (errorMessage) {
-  var messageEle = document.getElementById('message');
-  messageEle.style.color = 'red';
-  messageEle.innerText = errorMessage;
-});
+promise.then(function (responseText) {
+  document.getElementById('json').innerHTML = responseText;
+  console.log(JSON.parse(responseText))
+}).catch(function (error) {
+  console.log(error)
+})
