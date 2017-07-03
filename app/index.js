eb3fde6682b25e6af3872da8032e3adf25b54517
benweizhu <benweizhu@gmail.com>
2017-07-03 17:16:17 +0800

    chain

--- app/index.js
+++ app/index.js
@@ -1,26 +1,20 @@
-function fetchData(URL) {
-  return new Promise(function (resolve, reject) {
-    var req = new XMLHttpRequest();
-    req.open('GET', URL, true);
-    req.onload = function () {
-      if (req.status === 200) {
-        resolve(this.responseText);
-      } else {
-        reject(new Error(req.statusText));
-      }
-    };
-    req.onerror = function () {
-      reject(new Error(req.statusText));
-    };
-    req.send();
-  });
+function doubleUp(value) {
+  return value * 2;
+}
+function increment(value) {
+  return value + 1;
+}
+function output(value) {
+  console.log(value);// => (1 + 1) * 2
 }
 
-var promise = fetchData('https://raw.githubusercontent.com/benweizhu/es6-promise-workshop/master/data/books.json');
+var promise = Promise.resolve(1);
 
-promise.then(function (responseText) {
-  document.getElementById('json').innerHTML = responseText;
-  console.log(JSON.parse(responseText))
-}).catch(function (error) {
-  console.log(error)
-})
+promise
+  .then(increment)
+  .then(doubleUp)
+  .then(output)
+  .catch(function (error) {
+    // promise chain中出现异常的时候会被调用
+    console.error(error);
+  });
